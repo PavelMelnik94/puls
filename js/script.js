4e57fe4529804adb289c1319766b9bfdfa67f107
Pavel <recyclesordie94@gmail.com>
2021-01-11 13:01:30 +0300

    настроен php mailer
    отправка сверстаных email сообщений на мою почту(как на почту менеджера компании) готова.
    
    были проблемы с отправкой - включил доступ для НЕбезоспасных приложений в аккаунте гугла.
    
     - open server

--- js/script.js
+++ js/script.js
@@ -104,4 +104,21 @@ validateForms('#consultation form');
 validateForms('#order form');
 
 $('input[name=phone]').mask('+7 (99) 999-99-99');
+
+$('form').submit(function(e) {
+  e.preventDefault();
+  $.ajax({
+      type: "POST",
+      url: "mailer/smart.php",
+      data: $(this).serialize()
+  }).done(function() {
+      $(this).find("input").val("");
+      $('#consultation, #order').fadeOut();
+      $('.overlay, #thanks').fadeIn('slow');
+
+      $('form').trigger('reset');
+  });
+  return false;
+});
+
 });
